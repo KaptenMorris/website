be80e89287d0d72de55a49b5da9fffbce647b7bb
KaptenMorris <118316880+KaptenMorris@users.noreply.github.com>
2023-08-11 22:01:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript code can be added here
+// This is just a placeholder for demonstration purposes
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
